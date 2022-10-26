4b6eaad5bf15a01630c7aa504c3c34dd8016a1f6
Jacob <jacobsimoninielsen@gmail.com>
2022-10-26 11:30:41 +0200

    Arbejde Fra min hjemme PC
    Arbejde Fra min hjemme PC overført til min bærbar

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,27 +1,122 @@
-import { GoalModel } from "./goals.js"
+const section = document.querySelector("section");
+const playerLivesCounts = document.querySelector("span")
+let playerLives = 6;
 
-const gameboard = document.getElementById('gameboard')
-let cardList = GoalModel()
+playerLivesCounts.textContent = playerLives;
 
-//Beslutter hvor mange kort der skal være
-const numCards = 10;
+const getData = () => [
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_1.svg", name: "One" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_2.svg", name: "Two" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_3.svg", name: "Three" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_4.svg", name: "Four" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_5.svg", name: "Five" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_6.svg", name: "Six" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_7.svg", name: "Seven" },    
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_8.svg", name: "Eight" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_9.svg", name: "Nine" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_10.svg", name: "Ten" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_11.svg", name: "Eleven" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_12.svg", name: "Tvelve" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_13.svg", name: "Therteen" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_14.svg", name: "Fourteen" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_15.svg", name: "Fiveteen" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_16.svg", name: "Sixteen" },
+    {imgSrc: "./Assets/Images/TheGlobalGoals_Icons_Color_Goal_17.svg", name: "Seventeen" },
+];
+
+const randomize = () => {
+    var cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    cardData = cardData.filter((cardData,idx) => idx < 8)
+    cardData = cardData.concat(cardData)
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+};
 
 
-cardList.sort(() => Math.random() - 0.5); 
-cardList = cardList.slice(0, numCards);
-cardList = cardList.concat(cardList);
-cardList.sort(() => Math.random() - 0.5);
 
-for(let card of cardList) {
-    let div = document.createElement('div')
-    div.innerText = card.goal
-    div.classList.add('back')
-    gameboard.prepend(div)
+const cardGenerator = () => {
+    const cardData = randomize();
+    //html
+    cardData.forEach((item) => {    
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+    card.classList = "card";
+    face.classList = "face";
+    back.classList = "back";
+
+    face.src = item.imgSrc;
+    card.setAttribute("name", item.name)
+
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back)
+
+    card.addEventListener("click", (e) => {
+        card.classList.toggle("toggleCard")
+        checkCards(e)
+    });
+    });
+};
+
+const checkCards = (e) => {
+    const clickedCard = e.target;
+    clickedCard.classList.add("flipped"); 
+    const flippedCards = document.querySelectorAll(".flipped");
+    const toggleCard = document.querySelectorAll(".toggleCard")
+    console.log(flippedCards);
+
+    if(flippedCards.length === 2) {
+        if(
+        flippedCards[0].getAttribute('name') === 
+        flippedCards[1].getAttribute('name')
+        ) {
+            console.log("match");
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents = "none";
+            })
+        } else {
+            console.log("wrong");
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                // card.classList.remove("toggleCard");
+                setTimeout(() => card.classList.remove("toggleCard"), 1000);
+                // setTimeout(() => card.classList.remove("flipped"), 1000);
+            });
+            playerLives--;
+            playerLivesCounts.textContent = playerLives;
+            if(playerLives === 0) {
+                restart("Try again:/");
+            }
+        }
+    }
+    //win
+    if(toggleCard.length === 16) {
+        restart("You won!")
+    }
 }
 
+//restart
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents = "none";
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove("toggleCard")
 
+        setTimeout(() =>{
+            cards[index].style.pointerEvents = "all";
+            faces[index].src = item.imgSrc;
+            cards[index].setAttribute("name", item.name);
+            section.style.pointerEvents = "all";
+        }, 1000)
+    })
+    playerLives = 6;
+    playerLivesCounts.textContent = playerLives;
+    setTimeout(() => window.alert(text), 1000)
+}
 
-
-
-
-console.log(cardList);
+cardGenerator();
